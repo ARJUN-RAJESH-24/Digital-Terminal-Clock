647b954144602865a084ef69863d9badb5de9dcf
ARJUN RAJESH <arjunrajesh59@gmail.com>
2026-01-06 17:04:30 +0530

    Completed

---
+++ DigitalClock.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<stdbool.h>
+//#include<unistd.h>//for linux
+#include<Windows.h>
+
+/*
+
+time_t holds number of seconds,the type time_t is used for calculating the difference in time b/w Unix Epoch time i.e, Jan 1st 1970 and now
+
+struct tm is a struct in the time header file
+
+*/
+
+
+int main(void){
+    time_t rawtime = 0; 
+    struct tm *pTime = NULL;
+    bool isRunning = true; 
+    
+    printf("D I G I T A L   C L O C K \n");
+
+    while(isRunning){
+
+        time(&rawtime);
+        pTime = localtime(&rawtime);
+
+        printf("\r%02d:%02d:%02d",pTime->tm_hour,pTime->tm_min,pTime->tm_sec);
+        
+
+
+        // sleep(1);//for linux in sec
+        Sleep(1000);// for windows in milli-sec
+    }
+
+    return 0 ;
+}
